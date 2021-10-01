645008da22aaa9e91971508d0705270064f96ec5
Lauri Minotti <lauriminotti@gmail.com>
2021-10-01 16:59:28 -0300

    Carrito de compras

--- assets/almacenBalcarce/carrito.js
+++ assets/almacenBalcarce/carrito.js
@@ -1,3 +1,5 @@
+/* Esto se va  a ver al ingresar a la página*/
+
 function ingreso(){
  let ubicacion= prompt("¿Sos de San Justo? SI / NO");
 
@@ -11,4 +13,38 @@ function ingreso(){
  }
 }
 ingreso();
+
+/*Funciones en el carrito*/
+function validarDatos(){
+    const nombre= document.getElementById("nombreApellido").value;
+    if (nombre === ""){
+        alert("Tenes que ingresar su nombre");
+        return;
+    }
+   /* alert(nombre);*/
+
+    const numero= document.getElementById("telefono").value;
+    if (numero === ""){
+        alert("Tenes que ingresar tu número de teléfono");
+        return;
+    }
+    /*alert(numero);*/
+    const direccion= document.getElementById("direccion").value;
+    if (direccion === ""){
+        alert("Tenes que ingresar tu dirección");
+        return;
+    }
+    /*alert(direccion);*/
+validarWhatsApp(nombre, direccion);
+
+}
+
+function validarWhatsApp(nombre, direccion){
+    const almacen= "+5493498478319";
+    const texto= "Hola, soy " + nombre + " quiero pedir estos productos: . Mi dirección es " + direccion + ".";
+    const url= "https://api.whatsapp.com/send?phone="+almacen+"&text="+texto;
+    window.open(url);
+    alert(url);
+}
+
 
